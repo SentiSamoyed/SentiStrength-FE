a0e5c69cb14e41e5abd7631188743646ce2f2733
hcplantern <38579760+HCPlantern@users.noreply.github.com>
2023-04-07 17:22:10 +0800

    Dengyouliang/feat/improve (#10)
    * feat: 更改标题，删除env无用环境变量
    
    * feat: 调整结果表格样式
    
    * chore: 还原 .env
    
    * chore: 删除env无用变量
    
    * feat: 增加 csv 导出功能；修改表格样式

--- src/views/AnalyseView.vue
+++ src/views/AnalyseView.vue
@@ -4,7 +4,7 @@
 
     <AnalyseText ref='AnalyseText'>
       <template #text>
-        <el-input type='textarea' rows='4' placeholder='请输入文本' v-model='form.text'
+        <el-input type='textarea' :autosize='{ minRows: 4, maxRows: 10 }' placeholder='请输入文本' v-model='form.text'
                   style='margin: 1rem;' />
       </template>
       <template #button>
@@ -13,12 +13,17 @@
     </AnalyseText>
 
     <AnalyseResult>
+      <template #button>
+        <el-button type='primary' @click='exportCsv'>导出</el-button>
+      </template>
 
       <template #table>
-        <el-table :data='tableData' border v-loading='loading'>
-          <el-table-column prop='val1' label='Positive' width='180'></el-table-column>
-          <el-table-column prop='val2' label='Negative' width='180'></el-table-column>
-          <el-table-column prop='val3' label='Mode Value' width='180'></el-table-column>
+        <el-table :data='tableData' :table-layout='"auto"' v-loading='loading' height='600px'>
+          <el-table-column prop='id' label='Id' width='40px' />
+          <el-table-column prop='text' label='Text'></el-table-column>
+          <el-table-column prop='val1' label='Positive' width='100px'></el-table-column>
+          <el-table-column prop='val2' label='Negative' width='100px'></el-table-column>
+          <el-table-column prop='val3' label='Mode Value' width='110px'></el-table-column>
           <el-table-column prop='explain' label='Explain'></el-table-column>
         </el-table>
       </template>
@@ -35,6 +40,7 @@ import { reactive } from 'vue'
 import AnalyseText from '@/components/AnalyseText.vue'
 import AnalyseResult from '@/components/AnalyseResult.vue'
 import axios from 'axios'
+import { ExportToCsv } from 'export-to-csv'
 
 export default {
   name: 'HomeView',
@@ -51,6 +57,9 @@ export default {
   methods: {
     submit() {
       this.loading = true
+
+      let textArr = this.splitText(this.$data.form.text)
+      this.tableData = Array(textArr.length).fill().map((_, index) => ({ id: index + 1, text: textArr[index] }))
 
       let data = {
         text: this.$data.form.text,
@@ -69,9 +78,15 @@ export default {
         .then(res => {
           if (res.data.code !== 0) {
             window.alert('请求失败' + '\n' + res.data.msg)
+            this.loading = false
             return
           }
-          this.tableData = res.data.data
+          // set table data
+          res.data.data.forEach((item, index) => {
+            const targetObj = this.tableData[index]
+            Object.assign(targetObj, { val1: item.val1, val2: item.val2, val3: item.val3, explain: item.explain })
+          })
+
         })
         .catch(err => {
           console.log(err)
@@ -79,7 +94,29 @@ export default {
         }).finally(() => {
         this.loading = false
       })
+    },
+    splitText(text) {
+      return text.split('\n')
+    },
+    exportCsv() {
+      const options = {
+        showLabels: true,
+        showTitle: false,
+        filename: 'sentistrength-result-' + this.$refs.AnalyseOptions.form.mode,
+        useTextFile: false,
+        useBom: true,
+        useKeysAsHeaders: false,
+        headers: ['id', 'text', 'val1', 'val2', 'val3', 'explain']
+      }
+
+      const csvExporter = new ExportToCsv(options)
+
+      csvExporter.generateCsv(this.tableData).catch(err => {
+        console.log(err)
+        window.alert('导出失败' + '\n' + err.message)
+      })
     }
+
   }
 }
 </script>
