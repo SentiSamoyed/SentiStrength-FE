cb35879e6a7470656e3272459c28714b6594c7b6
hcplantern <hcplantern@foxmail.com>
2023-05-26 23:45:39 +0800

    feat: 初始化项目相关功能

--- src/components/repo/RepoInit.vue
+++ src/components/repo/RepoInit.vue
@@ -54,7 +54,6 @@ export default {
         name: ''
       },
       loading: false,    // 是否正在加载数据
-      dataLoaded: false, // 数据是否加载完成
       maxAttempts: 15,   // 最大轮询次数
       attemptCount: 0    // 当前轮询次数
     }
@@ -69,25 +68,23 @@ export default {
         }
       })
       this.$log.debug('current repo name list: ' + repoNameList)
-      let suggestions = repoNameList.filter(item => item.value.includes(queryString))
+      let suggestions = repoNameList.filter(item => item.value.includes(queryString, 0))
       this.$log.debug('suggestions: ' + suggestions)
       cb(suggestions)
     }
     ,
     handleSelect(item) {
-      this.selectedRepoName = item.value
       this.$log.info('selected repo path: ' + this.form.selectedRepoName)
     }
     ,
     handleChange(item) {
-      this.selectedRepoName = item.value
       this.$log.info('input repo path: ' + this.form.selectedRepoName)
     },
     // 设置 repo owner 和 repo name
     setRepo(repoFullName) {
-      let repoInfo = repoFullName.match(/^([\w-]+)\/([\w-]+)$/)
-      this.repo.owner = repoInfo[1]
-      this.repo.name = repoInfo[2]
+      let [, owner, name] = repoFullName.match(/^([\w-]+)\/([\w-]+)$/)
+      this.repo.owner = owner
+      this.repo.name = name
     },
     // 设置父组件的 repo 信息
     setParentRepo() {
@@ -103,7 +100,7 @@ export default {
       })
 
       let repoList = this.$parent.$data.repoList
-      let initialized = repoList.filter(item => item.fullName === this.selectedRepoName).length > 0
+      let initialized = repoList.some(item => item.fullName === this.form.selectedRepoName)
 
       this.setRepo(this.form.selectedRepoName)
 
@@ -133,44 +130,47 @@ export default {
       this.loading = true    // 显示加载圈
       this.attemptCount = 0  // 重置轮询计数器
       this.pollData() // 开始轮询数据
-    }
-    ,
-    pollData() {
-      // 达到最大轮询次数，停止轮询
-      if (this.attemptCount >= this.maxAttempts) {
-        this.$log.error('轮询超时')
-        this.$message.error('请求超时')
-        this.loading = false
-        return
-      }
-
-      apis.initRepo(this.repo.owner, this.repo.name).then(res => {
+    },
+    async pollData() {
+      try {
+        // 达到最大轮询次数，停止轮询
+        if (this.attemptCount >= this.maxAttempts) {
+          this.$log.error('轮询超时')
+          this.$message.error('请求超时')
+          this.loading = false
+          return
+        }
+        const res = await apis.initRepo(this.repo.owner, this.repo.name)
         let data = res.data
+
         this.$log.debug('轮询中...次数' + this.attemptCount)
         this.$log.info('轮询结果：' + data)
-        if (data.data === 0) {
+
+        if (data === 0) {
           // 未初始化完成，继续轮询
-          setTimeout(this.pollData, 2000)
+          await new Promise(resolve => setTimeout(resolve, 2000))
           this.attemptCount++
         } else if (data.data === 1) {
           // 初始化完成，停止轮询
           this.$log.info('轮询完成')
           this.loading = false
-          this.dataLoaded = true
           this.setParentRepo()
           this.$message.success('初始化成功')
         } else if (data.data === 2) {
           // repo 不存在
           this.$log.error('Repo 不存在')
+          this.loading = false
           this.$message.error('该仓库不存在')
         } else if (data.data === 3) {
           this.$log.error('Repo 不存在 Issues')
+          this.loading = false
           this.$message.error('该仓库不存在 Issues')
         }
-      }).catch(err => {
-        this.$log.error('请求失败: ' + err)
-        this.$message.error('请求失败')
-      })
+      } catch (e) {
+        this.$log.error('轮询异常: ', e)
+        this.loading = false
+        this.$message.error('请求异常')
+      }
     }
   }
 }
