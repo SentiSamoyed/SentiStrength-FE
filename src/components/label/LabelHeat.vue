8b4b9a81c76af0fa0a94a82d7b64777377c2b035
hcplantern <hcplantern@foxmail.com>
2023-06-02 13:47:24 +0800

    feat: 完成折线图和热力图

--- src/components/label/LabelHeat.vue
+++ src/components/label/LabelHeat.vue
@@ -1,11 +1,104 @@
 <template>
-  <div id='canvas'></div>
+  <card>
+    <template #icon-text>
+      <span>热力图</span>
+    </template>
+    <template #body>
+      <div id='heat-canvas'></div>
+    </template>
+  </card>
 </template>
 
 <script>
 
+import Card from '@/components/common/Card.vue'
+import { Heatmap } from '@antv/g2plot'
+import heatmap from '/static/json/heat_map_220221.json'
+
 export default {
-  name: 'LabelHeat'
+  name: 'LabelHeat',
+  components: { Card },
+  data() {
+    return {
+      plot: null
+    }
+  },
+  mounted() {
+    let data = heatmap
+    data.forEach((item) => {
+      item.size = parseFloat(item.size) * 10
+    })
+    // 按照 sentiment 和 volume 排序
+    data.sort((a, b) => {
+      if (a.sentiment === b.sentiment) {
+        return a.volume - b.volume
+      } else {
+        return a.sentiment - b.sentiment
+      }
+    })
+    this.draw(data)
+  },
+  methods: {
+    draw(data) {
+      this.$log.debug(data)
+      this.plot = new Heatmap(document.getElementById('heat-canvas'), {
+        data,
+        meta: {
+          volume: {
+            alias: '频数',
+            nice: true,
+            formatter: (text) => {
+              return parseInt(text)
+            }
+          },
+          sentiment: {
+            alias: '情感',
+            nice: true,
+            formatter: (text) => {
+              return parseFloat(text).toFixed(2)
+            }
+          },
+          aspect: {
+            alias: '方面',
+            nice: true
+          }
+        },
+        xField: 'volume',
+        yField: 'sentiment',
+        sizeField: 'size',
+        colorField: 'aspect',
+        shape: 'circle',
+        label: {
+          style: {
+            fill: '#fff',
+            stroke: '#fff',
+            strokeOpacity: 0.5,
+            lineWidth: 0.5,
+            shadowColor: 'rgba(0, 0, 0, 0.45)',
+            shadowBlur: 2
+          },
+          formatter: (text, item) => {
+            return (parseFloat(text.size)).toFixed(0)
+          }
+        },
+        tooltip: {
+          showTitle: false,
+          fields: ['aspect', 'volume', 'sentiment']
+        },
+        xAxis: {
+          title: {
+            text: '频数'
+          }
+        },
+        yAxis: {
+          title: {
+            text: '情感'
+          }
+        }
+      })
+      this.plot.render()
+    }
+  }
 }
 </script>
 
--- src/components/label/LabelLine.vue
+++ src/components/label/LabelLine.vue
@@ -1,10 +1,118 @@
 <template>
-
+  <card>
+    <template #icon-text>
+      <span>折线图</span>
+    </template>
+    <template #body>
+      <div id='line-canvas'></div>
+    </template>
+  </card>
 </template>
 
 <script>
+import Card from '@/components/common/Card.vue'
+import lineChart from '/static/json/linechart.json'
+import { Line } from '@antv/g2plot'
+
 export default {
-  name: 'LabelLine'
+  name: 'LabelLine',
+  components: { Card },
+  data() {
+    return {
+      plot: null
+    }
+  },
+  mounted() {
+    let data = lineChart
+    let v1Data = []
+    let v2Data = []
+    data.forEach((item) => {
+      if (item.version === 'rocketmq-all-4.9.3') {
+        v1Data.push(item)
+      }
+    })
+    data.forEach((item) => {
+      if (item.version === 'rocketmq-all-4.9.4') {
+        v2Data.push(item)
+      }
+    })
+    v1Data = this.completeXField(v1Data)
+    v2Data = this.completeXField(v2Data)
+    // 合并两个版本的数据
+    data = v1Data.concat(v2Data)
+    this.draw(data)
+  },
+  methods: {
+    completeXField(arr) {
+      // 补全缺失的 xField 值
+      // 找到最小的 xField 和最大的 xField
+      let minXField = arr[0].xField
+      let maxXField = arr[0].xField
+      arr.forEach(item => {
+        if (item.xField < minXField) {
+          minXField = item.xField
+        }
+        if (item.xField > maxXField) {
+          maxXField = item.xField
+        }
+      })
+      let completeData = []
+      for (let i = minXField; i <= maxXField; i++) {
+        const existingData = arr.find(item => item.xField === i)
+        if (existingData) {
+          completeData.push(existingData)
+        } else {
+          const newData = {
+            xField: i,
+            yField: 0, // 或者设置为其他默认值
+            aspect: 'no data', // 或者设置为其他默认值
+            version: arr[0].version // 或者设置为其他默认值
+          }
+          completeData.push(newData)
+        }
+      }
+      return completeData
+    },
+    draw(data) {
+      this.$log.debug('line chart data: ', data)
+      const line = new Line('line-canvas', {
+        data: data,
+        xField: 'xField',
+        yField: 'yField',
+        xAxis: {
+          title: {
+            text: '时间'
+          }
+        },
+        yAxis: {
+          title: {
+            text: '评论数量'
+          }
+        },
+        seriesField: 'version',
+        point: {}
+        // label: {
+        // text 是data列表中的某个Object，item 是每个点的数据，index 是索引
+        // formatter: (text, item, index) => {
+        //   if (this.yAxis === 'avg') {
+        //     return text.avg.toFixed(2)
+        //   } else if (this.yAxis === 'ratio') {
+        //     return (text.ratio * 100).toFixed(2) + '%'
+        //   } else if (this.yAxis === 'sum') {
+        //     return text.sum
+        //   }
+        // }
+        // },
+        // slider: {
+        //   start: 0.1,
+        //   end: 0.9
+        // }
+      })
+      this.plot = line
+      line.render()
+
+    }
+  }
 }
 </script>
 
